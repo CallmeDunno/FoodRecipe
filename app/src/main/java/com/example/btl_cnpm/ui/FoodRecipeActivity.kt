b5250da27b24c40c97f96a3bb62babab21214bee
Dunno <dungworkit@gmail.com>
2023-10-25 16:43:20 +0700

    edit base header, create bottom bar and change color primary variant

--- app/src/main/java/com/example/btl_cnpm/ui/FoodRecipeActivity.kt
+++ app/src/main/java/com/example/btl_cnpm/ui/FoodRecipeActivity.kt
@@ -2,11 +2,33 @@ package com.example.btl_cnpm.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.example.btl_cnpm.R
+import com.example.btl_cnpm.base.BaseActivity
+import com.example.btl_cnpm.databinding.FoodRecipeActivityMainBinding
+import com.example.btl_cnpm.utils.extensions.hide
+import com.example.btl_cnpm.utils.extensions.show
 
-class FoodRecipeActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.food_recipe_activity_main)
+class FoodRecipeActivity : BaseActivity<FoodRecipeActivityMainBinding>() {
+    override val layoutID = R.layout.food_recipe_activity_main
+
+    private var _navController: NavController? = null
+    private val navController get() = _navController!!
+
+    override fun initView() {
+        super.initView()
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        _navController = navHostFragment.navController
+        binding.bottomNavigation.setupWithNavController(navHostFragment.navController)
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            when (destination.id){
+                R.id.homeFragment,
+                R.id.bookmarkFragment ,
+                R.id.profileFragment -> binding.bottomNavigation.show()
+                else -> binding.bottomNavigation.hide()
+            }
+        }
     }
 }
--- app/src/main/java/com/example/btl_cnpm/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/btl_cnpm/ui/home/HomeFragment.kt
@@ -7,9 +7,9 @@ import android.view.View
 import android.view.ViewGroup
 import com.example.btl_cnpm.R
 import com.example.btl_cnpm.base.BaseFragment
+import com.example.btl_cnpm.databinding.FoodRecipeFragmentHomeBinding
 
-class HomeFragment : Fragment() {
-
-
+class HomeFragment : BaseFragment<FoodRecipeFragmentHomeBinding>() {
+    override val layoutId = R.layout.food_recipe_fragment_home
 
 }
