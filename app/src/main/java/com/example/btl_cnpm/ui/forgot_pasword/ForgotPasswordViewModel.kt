121e2bacb21cf5fd30498b95e929173c46733978
The Girl <phamdungabcdef@gmail.com>
2023-10-29 00:43:29 +0700

    create a few ViewModel and Repository files

---
+++ app/src/main/java/com/example/btl_cnpm/ui/forgot_pasword/ForgotPasswordViewModel.kt
@@ -0,0 +1,8 @@
+package com.example.btl_cnpm.ui.forgot_pasword
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+
+@HiltViewModel
+class ForgotPasswordViewModel : ViewModel() {
+}
---
+++ app/src/main/java/com/example/btl_cnpm/ui/sign_up/SignUpViewModel.kt
@@ -0,0 +1,8 @@
+package com.example.btl_cnpm.ui.sign_up
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+
+@HiltViewModel
+class SignUpViewModel : ViewModel() {
+}
