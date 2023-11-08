b3150356854b06df1e378a4b176619314da12319
Dunno <dungworkit@gmail.com>
2023-11-08 23:27:37 +0700

    Fix UX

--- app/src/main/java/com/example/btl_cnpm/ui/recipe/RecipeFragment.kt
+++ app/src/main/java/com/example/btl_cnpm/ui/recipe/RecipeFragment.kt
@@ -143,9 +143,6 @@ class RecipeFragment : BaseFragment<FoodRecipeFragmentRecipeBinding>() {
                     popUp.show()
                 }
             }
-            btnFollowRecipe.setOnClickListener {
-
-            }
         }
     }
 
